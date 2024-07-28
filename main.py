3311ad737425ffde1dcc367f742c2fe7cd3dcf7b
ryomashita <86085692+ryomashita@users.noreply.github.com>
2024-07-28 18:47:46 +0900

    update main.py (tutorial: path parameter)

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from enum import Enum # 標準ライブラリ
+
 from fastapi import FastAPI
 
 # run with: 
@@ -13,8 +15,42 @@ async def my_root():
     # define response(cocntent)
     return {"message": "Hello World"}
 
+# 固定パスは parameter path よりさきに定義する
+@my_app.get("/items/me")
+async def read_item_me():
+    return {"item_id": "the current user's id"}
+
 @my_app.get("/items/{item_id}") # path parameter
 async def read_item(item_id: int):
     # FastAPI は Python型宣言によりvalidationを行う
     # "/items/4.2" へのアクセスはエラーレスポンスが返る
     return {"item_id": item_id}
+
+# --- Enum parameters ---
+
+# str 型の enum を定義
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+# enum 型の path parameter
+@my_app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    # is (==) か .value
+    if model_name is ModelName.alexnet:
+        # Enum をそのまま返せる
+        # (FastAPI が定義型 str に変換する)
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+# --- filepath parameters ---
+
+# `:path` は任意のパス形式にマッチする
+# `/files//home/myfile.txt` など スラッシュが連続する形になる
+@my_app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
