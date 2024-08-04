b51caa36a61abb4561c947f31433beef8013d190
ryomashita <86085692+ryomashita@users.noreply.github.com>
2024-08-04 16:42:19 +0900

    update examples (tutorial: request_file)

--- request_file.py
+++ request_file.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
@@ -17,9 +18,39 @@ async def create_file(file: Annotated[bytes, File()]):
 # (制限を超えた分はファイルに保存される)
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
+    # await : 非同期処理(async)の終了を待機する
     read_file = await file.read(10000)
     return {
         "filename": file.filename,
         "content_type": file.content_type,
         "read_file": read_file.__sizeof__(),
     }
+
+@app.post("/uploadfile/")
+async def create_upload_file(
+    # UploadFile でも Annotated でメタデータを追加できる
+    file: Annotated[UploadFile, File(description="A file read as UploadFile")],
+):
+    return {"filename": file.filename}
+
+# 複数ファイルを受け取る場合 : list[UploadFile]
+@app.post("/uploadfiles/")
+async def create_upload_files(files: list[UploadFile]):
+    return {"filenames": [file.filename for file in files]}
+
+# ファイルを送信する HTML フォーム例
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
